2949978327a9c888eed52c605a81d95e877ddd1b
Marek Sedlacek <sedlacek.marek@gmail.com>
2022-10-21 11:07:55 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from datetime import timedelta
+
+def date_fnc(hours_shift):
+    now_t = datetime.now()
+    delta_h = timedelta(hours=hours_shift)
+    new_time = now_t + delta_h
+    return new_time
+
+
+def t_printer(dt_obj):
+    print(f"Current time is: {dt_obj}")
+
+
+if __name__ == "__main__":
+    to = date_fnc(1)
+    t_printer(to)

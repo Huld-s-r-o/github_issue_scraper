5bbbaae9663cacd94e580ed69eecd4dd3144918f
Marek Sedlacek <sedlacek.marek@gmail.com>
2022-12-04 23:21:03 +0100

    Adding a Sphinx documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+project = 'Github issues scraper'
+copyright = '2022, Huld (MSe)'
+author = 'Huld (MSe)'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+]
+
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+
+napoleon_google_docstring = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+autosummary_generate = True
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'classic'
+html_static_path = ['_static']
---
+++ docs/source/generated/main.rst
@@ -0,0 +1,40 @@
+﻿main
+====
+
+.. automodule:: main
+
+   
+   
+   
+
+   
+   
+   .. rubric:: Functions
+
+   .. autosummary::
+   
+      date_fnc
+      make_path
+      save_csv
+      save_json
+      t_printer
+      transformation_v1
+   
+   
+
+   
+   
+   .. rubric:: Classes
+
+   .. autosummary::
+   
+      GithubRepo
+   
+   
+
+   
+   
+   
+
+
+

df09b3d3c9464d4b2656706ec9b3df77671eff4f
Marek Sedlacek <sedlacek.marek@gmail.com>
2022-12-04 19:01:53 +0100

    Adding documentation and updating compose together with configurable repository input from command line

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# playground
+# Scraper of GitHub issues via API
+
+It stores given Github repository into .csv and .json files with transformation, i.e selecting only some details about issue into repository.
+Original purpose is to make transition from GitHub into Jira issue easy. However by adding some other transformation functions, the purpose of this scrapper can easily extended.
+
+
+RUN as 
+'''
+GITHUB_ADDRESS="https://github.com/<repo_owner>/<repo_name>" docker-compose up --build
+'''
+
+or
+
+'''
+GITHUB_ADDRESS="https://github.com/<repo_owner>/<repo_name>" docker-compose run api_issues
+'''
--- main.py
+++ main.py
@@ -5,16 +5,22 @@ from os import path
 from os import makedirs
 from os import getenv
 
+import re
 import urllib.request
 import urllib.parse
 import json
 import csv
+import sys
 
 def date_fnc(hours_shift):
-    '''
-    Date function return date object shifted about
-    hours_shift param
-    '''
+    """Date function returning date object shifted about hours_shift arg
+
+    Args:
+        hours_shift (int): number of hours for shifting now date
+
+    Returns:
+        date: shifted date object
+    """
     now_t = datetime.now()
     delta_h = timedelta(hours=hours_shift)
     new_time = now_t + delta_h
@@ -22,17 +28,30 @@ def date_fnc(hours_shift):
 
 
 def t_printer(dt_obj):
+    """Date obj printer
+
+    Args:
+        dt_obj (date): date for console print
+    """
     print(f"Current time is: {dt_obj}")
 
 
 class GithubRepo():
-    '''
-    Class representing Github repository and wrappers
-    on basic Github API.
-    '''
+    """Class representing Github repo with set of wrappers for GitHub API
+
+    Returns:
+        _type_: GithubRepo instance
+    """
+    
     api_repo_url = "https://api.github.com/repos/"
 
     def __init__(self, owner, repo):
+        """Instantiate Github repository class with owner and repo name
+
+        Args:
+            owner (_type_): owner of repository
+            repo (_type_): repository name
+        """
         self.owner = owner
         self.repo_name = repo
 
@@ -40,6 +59,11 @@ class GithubRepo():
         return f"{self.api_repo_url}{self.owner}/{self.repo_name}"
 
     def get_access_token(self):
+        """Returns access token from ENV variable GITHUB_TOKEN or from pre-defined file "access_token.priv"
+
+        Returns:
+            _type_: _description_
+        """
         token = ""
         if getenv("GITHUB_TOKEN") is None:
             with open("access_token.priv",'r') as f:
@@ -50,10 +74,16 @@ class GithubRepo():
         return token
 
     def __str__(self):
+        """String representation of GithubRepo class
+
+        Returns:
+            _type_: string with owner and repository name for given instance.
+        """
         return f"Github repository of OWNER: {self.owner}, REPO_NAME:{self.repo_name}"
 
     def download_issues_all(self):
-        
+        """Downloads all issues from Github and store them as list of dictionaries
+        """
         last_page = 1
         url_query = [('per_page',50)]
         encoded_query = urllib.parse.urlencode(url_query)
@@ -62,18 +92,24 @@ class GithubRepo():
         request_obj = urllib.request.Request(url=url+encoded_query,headers={'Authorization' : f'token {access_token}'})
 
         with urllib.request.urlopen(request_obj) as w:
+            #Check whether there are multiple pages with issues
              print(f"{w.url} => response status: {w.status}, {w.reason} ")
              print(f"{w.getheaders()}")
-             link_header = w.getheader('Link').split(',')
-             last_only = [ item for item in link_header if item.find('rel="last"') > 0]
-             clean_url_last=last_only[0].split(';')[0].strip().replace('<','').replace('>','')
-             parsed_url = urllib.parse.urlparse(clean_url_last)
-             dict_qp = urllib.parse.parse_qs(parsed_url.query)
-             last_page = int(dict_qp.get('page',1)[0])
+             link_header_all = w.getheader('Link')
+             last_page = 1
+             if link_header_all is not None:
+                link_header = link_header_all.split(',')
+                last_only = [ item for item in link_header if item.find('rel="last"') > 0]
+                clean_url_last=last_only[0].split(';')[0].strip().replace('<','').replace('>','')
+                parsed_url = urllib.parse.urlparse(clean_url_last)
+                dict_qp = urllib.parse.parse_qs(parsed_url.query)
+                last_page = int(dict_qp.get('page',1)[0])
+            
              print(f"Last page: {last_page}")
         
         resp_data = []
-        for pp in range(1,last_page + 1):            
+        for pp in range(1,last_page + 1):
+            #Loop through pages with issues            
             encoded_query = urllib.parse.urlencode([*url_query,('page',pp)])
             request_obj = urllib.request.Request(url=url+encoded_query,headers={'Authorization' : f'token {access_token}'})
             print(f"Querying page:{pp}, {request_obj.full_url} ")
@@ -84,6 +120,11 @@ class GithubRepo():
         self.issues_all = resp_data
 
     def get_issues_only(self):
+        """Getter for issues from given Github repository ommitting pull requests (treated by Github also as an issue)
+
+        Returns:
+            list: list issues of in the repository
+        """
         if not(hasattr(self,'issues_all')):
             self.download_issues_all()    
         
@@ -91,18 +132,39 @@ class GithubRepo():
     
    
     def get_transformed_issues(self,trans_fnc):
+        """Transformation of issue details according to provided function.
+
+        Args:
+            trans_fnc (fnc): function transforming issue details into desired output (i.e. stripping unwanted info and changing structure optimal for further processing)
+
+        Returns:
+            list: list of transformed issues 
+        """
         transformed = list(map( trans_fnc, self.get_issues_only()))
         
         return transformed
 
 def make_path(filename):
+    """wrapper for making output folder in the current dir and return the path appended by given filename
+
+    Args:
+        filename (string): filename to append to output path
+
+    Returns:
+        string: complete path of file on output
+    """
     out_dir="outputs"
     makedirs(out_dir,exist_ok=True)
     return path.join(out_dir,filename)
 
 
 def save_csv(filename, data_dict):
-    
+    """Save data into given file formatted as csv
+
+    Args:
+        filename (_type_): filename of output
+        data_dict (_type_): data as dictionary
+    """
     with open(make_path(filename),'w',encoding='utf-8', newline='') as f:
         wo = csv.writer(f, quoting=csv.QUOTE_ALL)
         count = 0
@@ -116,11 +178,39 @@ def save_csv(filename, data_dict):
 
 
 def save_json(filename, data_dict):
+    """Save data into given file formatted as json
+
+    Args:
+        filename (_type_): filename of output
+        data_dict (_type_): data as dictionary
+    """
     with open(make_path(filename),'w',encoding='utf-8') as f:
         json.dump(data_dict, f)
     print(f"Saved {len(data_dict)} issues into: {filename}")
 
 def transformation_v1(element):
+    """Concrete transformation of issues into a structure where only:
+       - number
+       - title
+       - n-comments
+       - body of issues as text
+       - updated
+       - created
+       - state 
+       - labels
+       - milestone title
+       - html url of issue
+       - url of issue
+     are kept for output.
+
+
+
+    Args:
+        element (dict): single issue with all details as got from GitHub API
+
+    Returns:
+        dict: transformed issue
+    """
     return {
         'number' : element ['number'], 
         'title' : element['title'],
@@ -137,10 +227,42 @@ def transformation_v1(element):
 
 
 if __name__ == "__main__":
+    args=sys.argv[1:]
+    nargs=len(args)
+
+    repo_owner=""
+    repo_name=""
+
+    if nargs >= 1:
+        if nargs == 1:
+            #Consider 1st command-line argument as github.com repo address, i.e https://github.com/owner/repo-name
+            address=sys.argv[1]
+            res = re.search('.*(github.com)(.*)', address)
+            if res is None:
+                #Error if github.com address not found
+                print(f"The input argument {sys.argv[1]} isn't github.com address => fix to this format https://github.com/repo_owner/repo_name")
+                sys.exit(1)
+            repo_details=res.group(2).split("/")
+            if len(repo_details) != 3 or repo_details[2] == '':
+                #Error if github.com address doesn't contain repo_owner and repo_name separated by /
+                print(f"Input address has to be in this format => https://github.com/repo_owner/repo_name")
+                sys.exit(1)
+            repo_owner=repo_details[1]
+            repo_name=repo_details[2]
+        else:
+            #Consider 1st, 2nd command-line argument as owner and repository name on GitHub.com
+            repo_owner=sys.argv[1]
+            repo_name=sys.argv[2]
+
+    print(f"Repo-owner: {repo_owner}, Repo-name: {repo_name}")
+    if repo_owner == "" and repo_name == "":
+        print(f"Repository details not obtained => Exiting")
+        sys.exit(1)
+    
     to = date_fnc(0)
     date_frmt = to.strftime("%Y%m%d%H%M%S")
     t_printer(date_frmt)
-    repo_amcr = GithubRepo("ARUP-CAS","aiscr-webamcr")
+    repo_amcr = GithubRepo(repo_owner,repo_name)
     data = repo_amcr.get_transformed_issues(transformation_v1)
     print(f"Received {len(data)} issues")
     
